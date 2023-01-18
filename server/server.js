50b0bff821f57c2ef8cbde3382ea78864a398a73
JCaloca <jacobcaloca1@gmail.com>
2023-01-18 13:03:01 -0800

    Required authMiddleware function from /utils, changed app.get to catch all

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
-const { typeDefs, resolvers } = require('./schemas');
+// Database connection
 const db = require('./config/connection');
+// Import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+// Import typedefs and resolvers from schemas
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Setting up Apollo server and its schema
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -20,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
